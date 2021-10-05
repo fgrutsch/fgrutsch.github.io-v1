970552aadf41276047877e1425baa14eb11c87fe
Fabian Grutsch <f.grutsch@gmail.com>
2021-10-05 16:57:06 +0200

    Updates

---
+++ precache-manifest.87a0bd99414d03ddcb74fb68f54633a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "591e003cdc1ad01083531d7497335356",
+    "url": "/index.html"
+  },
+  {
+    "revision": "21e1b1587751904c39db",
+    "url": "/static/css/main.b5792bd0.chunk.css"
+  },
+  {
+    "revision": "04ca32067a813f972c77",
+    "url": "/static/js/2.e3432f5f.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/static/js/2.e3432f5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "21e1b1587751904c39db",
+    "url": "/static/js/main.7b9b5f83.chunk.js"
+  },
+  {
+    "revision": "f52ba179432c3be28857",
+    "url": "/static/js/runtime-main.554504a0.js"
+  },
+  {
+    "revision": "7b560090ab97fe280437010d4a6df078",
+    "url": "/static/media/fabiangrutsch.7b560090.jpg"
+  }
+]);
